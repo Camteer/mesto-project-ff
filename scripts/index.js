e97e33eb43716d676f40757bcbde9c14b3c135e8
Pavlov Evgenii <pavlov2001@bk.ru>
2024-02-09 19:23:50 +0300

    v1.0.0

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,25 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const cardsContainer = document.querySelector('.places__list');
+
+function handleDeleteCard (element) {
+  element.target.parentElement.remove();
+}
+
+function addCard(element, def) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').setAttribute('src', element.link);
+  cardElement.querySelector('.card__image').setAttribute('alt', `Картинка местности ${element.name}`)
+  cardElement.querySelector('.card__title').textContent = element.name;
+  
+  cardElement.querySelector('.card__delete-button').addEventListener('click', def);
+  return cardsContainer.append(cardElement);
+};
+
+initialCards.forEach(function (element) {
+  addCard(element, handleDeleteCard)
+});
